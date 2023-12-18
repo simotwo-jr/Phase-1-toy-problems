457c092925743a336a6316b8386886a42d2d4ae2
Nehemiah Cheruiyot <nehemiahcheruiyot7069@gmail.com>
2023-12-18 22:47:03 +0300

    Checking range validity

--- grade.js
+++ grade.js
@@ -1,4 +1,10 @@
 function calculateGrade() {
         // Get user input for student marks
     const marks = parseFloat(prompt("Enter student marks (between 0 and 100):"));
+
+      // Check if the input is within the valid range
+      if (isNaN(marks) || marks < 0 || marks > 100) {
+        return "Invalid input. Marks should be between 0 and 100.";
+    }
+
 }
