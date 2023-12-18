23765603f458442d65de8b55541e1cfb54be98e9
Nehemiah Cheruiyot <nehemiahcheruiyot7069@gmail.com>
2023-12-18 23:46:48 +0300

    Calculating the net salary

---
+++ net-salary.js
@@ -0,0 +1,42 @@
+function calculateNetSalary(basicSalary, benefits) {
+    const taxRate = 0.3; 
+    const nhifRate = 0.05; 
+    const nssfRate = 0.06; 
+
+    // Calculate gross salary
+    const grossSalary = basicSalary + benefits;
+
+    // Calculate payee (tax)
+    const payee = grossSalary * taxRate;
+
+    // Calculate NHIF deductions
+    const nhifDeductions = grossSalary * nhifRate;
+
+    // Calculate NSSF deductions
+    const nssfDeductions = grossSalary * nssfRate;
+
+    // Calculate net salary
+    const netSalary = grossSalary - payee - nhifDeductions - nssfDeductions;
+
+    // Return the results
+    return {
+        grossSalary,
+        payee,
+        nhifDeductions,
+        nssfDeductions,
+        netSalary
+    };
+}
+
+// Testing the values
+const testBasicSalary = 50000; 
+const testBenefits = 10000;
+
+const salaryDetails = calculateNetSalary(testBasicSalary, testBenefits);
+
+// Display the results
+console.log("Gross Salary:", salaryDetails.grossSalary);
+console.log("Payee (Tax):", salaryDetails.payee);
+console.log("NHIF Deductions:", salaryDetails.nhifDeductions);
+console.log("NSSF Deductions:", salaryDetails.nssfDeductions);
+console.log("Net Salary:", salaryDetails.netSalary);
